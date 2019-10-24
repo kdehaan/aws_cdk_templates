12e34692922af95b200138406adb2366a8e58c52
Kevin de Haan <kbehaan@gmail.com>
2019-10-23 20:55:05 -0600

    lesson 3 up to date

--- lib/new-stack.ts
+++ lib/new-stack.ts
@@ -1,18 +1,21 @@
-import sns = require('@aws-cdk/aws-sns');
-import subs = require('@aws-cdk/aws-sns-subscriptions');
-import sqs = require('@aws-cdk/aws-sqs');
-import cdk = require('@aws-cdk/core');
+import cdk = require("@aws-cdk/core");
+import s3 = require("@aws-cdk/aws-s3");
 
 export class NewStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const siteSubDomain = "www";
+    const domain = "kdehaan-static-site.com";
+    const siteDomain = siteSubDomain + "." + domain;
 
-    const queue = new sqs.Queue(this, 'NewQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const siteBucket = new s3.Bucket(this, "SiteBucket1", {
+      bucketName: siteDomain,
+      websiteIndexDocument: "index.html",
+      websiteErrorDocument: "error.html",
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
     });
 
-    const topic = new sns.Topic(this, 'NewTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    new cdk.CfnOutput(this, "Bucket", { value: siteBucket.bucketName });
   }
 }
